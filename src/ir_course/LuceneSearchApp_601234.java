0c0f3a8cb2856f5547e32dee4871693c459e0df7
luckymj <ryuminjung@gmail.com>
2017-05-05 00:18:41 +0300

    Make basic configurations

--- src/ir_course/LuceneSearchApp_601234.java
+++ src/ir_course/LuceneSearchApp_601234.java
@@ -38,7 +38,7 @@ public class LuceneSearchApp_601234 {
 
 	}
 	
-	public void index(List<DocumentInCollection> docs) throws IOException {
+	public IndexWriterConfig index(List<DocumentInCollection> docs) throws IOException {
 
 		// implement the Lucene indexing here
 		
@@ -65,20 +65,15 @@ public class LuceneSearchApp_601234 {
 //			iwriter.addDocument(doc);
 //		}
 		
-		String method = "VSM";
-		boolean stopwords = true;
-		boolean stemmer = true;
+
+		//VSM + stopwords + stemmer
+		CharArraySet stopWords = EnglishAnalyzer.getDefaultStopSet(); //EnglishAnalyzer already has a stemmer
+		analyzer = new EnglishAnalyzer(stopWords);
+        config = new IndexWriterConfig(analyzer);
+        config.setSimilarity(new ClassicSimilarity());
+
 		
-		if (method.equals("VSM") && stopwords && stemmer )
-		{
-			//VSM + stopwords + stemmer
-			CharArraySet stopWords = EnglishAnalyzer.getDefaultStopSet(); //EnglishAnalyzer already has a stemmer
-			analyzer = new EnglishAnalyzer(stopWords);
-            config = new IndexWriterConfig(analyzer);
-            config.setSimilarity(new ClassicSimilarity());
-		}
-		
-		// Standard analyzer : porter stemming without stop words
+		// Standard analyzer : stop words - porter stemmer
 		
 
 		IndexWriter iwriter = new IndexWriter(directory, config);
@@ -88,21 +83,28 @@ public class LuceneSearchApp_601234 {
             Document doc = new Document();
 			doc.add(new Field("Title", documentdoc.getTitle(), TextField.TYPE_STORED));
 			doc.add(new Field("abstractText", documentdoc.getAbstractText(), TextField.TYPE_STORED));
-
-//            doc.add(new TextField("title", "doc" + doccount, Field.Store.YES));
-//            doc.add(new TextField("abstractText", documentdoc.getAbstractText(), Field.Store.YES));
             iwriter.addDocument(doc);
             doccount++;
         }		
 		
 		iwriter.close();
 		directory.close();
+		
+		return config;
 	}
 	
-	public List<String> search(List<String> searchQuery) throws IOException, ParseException {
-		
-		printQuery(searchQuery);
+	public List<String> search(String searchQuery, IndexWriterConfig cf) throws IOException, ParseException {
 
+		System.out.print("searchQuery: " + searchQuery);
+		
+		QueryParser qp = new QueryParser("abstractText", cf.getAnalyzer());
+		Query stemmedQuery = null;
+		try {
+			stemmedQuery = qp.parse(searchQuery);
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		
 		List<String> results = new LinkedList<String>();
 
 		// implement the Lucene search here
@@ -111,9 +113,16 @@ public class LuceneSearchApp_601234 {
 		// System.out.println("Docs: " + ireader.getDocCount("Title"));
 		IndexSearcher isearcher = new IndexSearcher(ireader);
 		
-		isearcher.setSimilarity(config.getSimilarity());
+		isearcher.setSimilarity(cf.getSimilarity());
 		
+		TopDocs docs = isearcher.search(stemmedQuery, 10);
+		ScoreDoc[] scored = docs.scoreDocs;
 		
+		for (ScoreDoc aDoc : scored) {
+			Document d = isearcher.doc(aDoc.doc);
+			results.add("+ " + d.get("title") + " | abstractText: " + d.get("abstractText") + " | score: " + aDoc.score);
+		}
+
 //		BooleanQuery.Builder booleanQuery = new BooleanQuery.Builder();
 //		
 //		if ( inAbstractText != null && !inAbstractText.isEmpty() ) {
@@ -146,25 +155,27 @@ public class LuceneSearchApp_601234 {
 		
 		
 		//System.out.println("Query: " + booleanQuery.build().toString());
-		ScoreDoc[] hits = isearcher.search(booleanQuery.build(), 1000).scoreDocs;	
-		for (int i = 0; i < hits.length; i++) {
-			Document hitDoc = isearcher.doc(hits[i].doc);
-			String feedTitle = hitDoc.get("Title");
-			results.add(feedTitle);
-		}
+//		ScoreDoc[] hits = isearcher.search(booleanQuery.build(), 1000).scoreDocs;	
+//		for (int i = 0; i < hits.length; i++) {
+//			Document hitDoc = isearcher.doc(hits[i].doc);
+//			String feedTitle = hitDoc.get("Title");
+//			results.add(feedTitle);
+//		}
 
+
+		
 		ireader.close();
 		
 		return results;
 	}
 	
-	public void printQuery(List<String> inAbstractText) {
-		System.out.print("Search (");
-		if (inAbstractText != null) {
-			System.out.print("in abstractText: "+inAbstractText);
+//	public void printQuery(List<String> inAbstractText) {
+//		System.out.print("Search (");
+//		if (inAbstractText != null) {
+//			System.out.print("in abstractText: "+inAbstractText);
 //			if (notInTitle != null || inDescription != null || notInDescription != null || startDate != null || endDate != null)
 //				System.out.print("; ");
-		}
+//		}
 //		if (notInTitle != null) {
 //			System.out.print("not in title: "+notInTitle);
 //			if (inDescription != null || notInDescription != null || startDate != null || endDate != null)
@@ -187,9 +198,9 @@ public class LuceneSearchApp_601234 {
 //		}
 //		if (endDate != null)
 //			System.out.print("endDate: "+endDate);
-		
-		System.out.println("):");
-	}
+//		
+//		System.out.println("):");
+//	}
 	
 	public void printResults(List<String> results) {
 		if (results.size() > 0) {
@@ -207,61 +218,20 @@ public class LuceneSearchApp_601234 {
 			
 			DocumentCollectionParser parser = new DocumentCollectionParser();
 			parser.parse(args[0]);
-			List<DocumentInCollection> docs = parser.getDocuments()
+			List<DocumentInCollection> docs = parser.getDocuments();
 				
-			engine.index(docs);
+			IndexWriterConfig cf = engine.index(docs);
 
-			List<String> inTitle;
-			List<String> notInTitle;
-			List<String> inDescription;
-			List<String> notInDescription;
 			List<String> results;
 			
+		
 			// 1) search documents with words "kim" and "korea" in the title
-			inTitle = new LinkedList<String>();
-			inTitle.add("kim");
-			inTitle.add("korea");
-			results = engine.search(inTitle, null, null, null, null, null);
+			results = engine.search("speech", cf);
 			engine.printResults(results);
 			
-			// 2) search documents with word "kim" in the title and no word "korea" in the description
-			inTitle = new LinkedList<String>();
-			notInDescription = new LinkedList<String>();
-			inTitle.add("kim");
-			notInDescription.add("korea");
-			results = engine.search(inTitle, null, null, notInDescription, null, null);
-			engine.printResults(results);
 
-			// 3) search documents with word "us" in the title, no word "dawn" in the title and word "" and "" in the description
-			inTitle = new LinkedList<String>();
-			inTitle.add("us");
-			notInTitle = new LinkedList<String>();
-			notInTitle.add("dawn");
-			inDescription = new LinkedList<String>();
-			inDescription.add("american");
-			inDescription.add("confession");
-			results = engine.search(inTitle, notInTitle, inDescription, null, null, null);
-			engine.printResults(results);
-			
-			// 4) search documents whose publication date is 2011-12-18
-			results = engine.search(null, null, null, null, "2011-12-18", "2011-12-18");
-			engine.printResults(results);
-			
-			// 5) search documents with word "video" in the title whose publication date is 2000-01-01 or later
-			inTitle = new LinkedList<String>();
-			inTitle.add("video");
-			results = engine.search(inTitle, null, null, null, "2000-01-01", null);
-			engine.printResults(results);
-			
-			// 6) search documents with no word "canada" or "iraq" or "israel" in the description whose publication date is 2011-12-18 or earlier
-			notInDescription = new LinkedList<String>();
-			notInDescription.add("canada");
-			notInDescription.add("iraq");
-			notInDescription.add("israel");
-			results = engine.search(null, null, null, notInDescription, null, "2011-12-18");
-			engine.printResults(results);
 		}
 		else
-			System.out.println("ERROR: the path of a RSS Feed file has to be passed as a command line argument.");
+			System.out.println("ERROR: the path of a document file has to be passed as a command line argument.");
 	}
 }
